b600677fba483f80f6df1c5b11278efbad108d9e
hminn <hmkim0008@gmail.com>
2021-07-28 23:56:12 +0900

    section9. 웹 API 서버 만들기
    - API 서버 이해하기
    - 프로젝트 구조 갖추기
    - JWT 토큰으로 인증하기
    - 호출 서버 만들기
    - SNS API 서버 만들기
    - 사용량 제한 구현하기
    - CORS 이해하기

--- section9/nodebird-api/routes/middlewares.js
+++ section9/nodebird-api/routes/middlewares.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const RateLimit = require('express-rate-limit');
 
 exports.isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) {
@@ -21,6 +22,7 @@ exports.verifyToken = (req, res, next) => {
   try {
     // 미들웨어 간의 데이터 전달 방식
     req.decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+    console.log(req.decoded);
     return next();
   } catch (error) {
     if (error.name === 'TokenExpiredError') { // 유효기간 초과
@@ -35,3 +37,22 @@ exports.verifyToken = (req, res, next) => {
     });
   }
 };
+
+exports.apiLimiter = new RateLimit({
+  windowMs: 60 * 1000, // 해당 시간 내에
+  max: 10, // 최대 max 번까지만 요청 가능
+  delayMs: 0, // 요청의 간격은 delayMs 이상
+  handler(req, res) {
+    res.status(this.statusCode).json({ // code : 429
+      code: this.statusCode,
+      message: '1분에 열 번만 요청할 수 있습니다.'
+    });
+  },
+});
+
+exports.deprecated = (req, res) => {
+  res.status(410).json({
+    code: 410,
+    message: '새로운 버전이 나왔습니다. 새로운 버전을 사용하세요.'
+  });
+}
